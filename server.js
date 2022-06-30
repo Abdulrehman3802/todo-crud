ea45fd79414a4b65d5ffa7108118a36e81dd846c
Abdulrehman3802 <108468341+Abdulrehman3802@users.noreply.github.com>
2022-06-30 17:34:45 +0500

    Port change

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ mongoose.connect(DB,{
     console.log ("DATA BASE ATTACHED")
 })
 
-const port=3000
+const port=process.env.PORT || 3000
 app.listen(port,()=>{
     console.log("server active now")
 })
